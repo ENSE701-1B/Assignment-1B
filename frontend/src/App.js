764fdeceb7c6cecc2f2179c3f5663a3271bcbaaa
Jiali Lu <jialilyv@gmail.com>
2022-05-24 11:52:18 +0800

    db connected & project up and running

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,51 @@
-import React, { Component } from 'react';
+import React from "react";
+import {
+  Route,
+  NavLink,
+  BrowserRouter as Router,
+  Redirect,
+} from "react-router-dom";
+import Home from "./pages/Home";
+import SEPractice from "./pages/SE-Practice";
+import SubmitArticle from "./pages/Submit-Article";
+import TableTemplate from "./pages/TableTemplate";
+import ModeratorPage from "./pages/ModeratorPage";
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      count: 0,
-    }
-  }
-
-  makeIncrementer = amount => () =>
-    this.setState(prevState => ({
-      count: prevState.count + amount,
-    }));
-
-  increment = this.makeIncrementer(1);
-
-  render() {
-    return (
+const App = () => {
+  
+  return (
+    <Router>
       <div>
-        <p>Count: {this.state.count}</p>
-        <button className="increment" onClick={this.increment}>Increment count</button>
+        <h1>Software Engineering Practice Evidence Repository (SEPER)</h1>
+        <ul className="header">
+          <li>
+            <NavLink exact to="/">
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/SEPractice">Select the Practice</NavLink>
+          </li>
+          <li>
+            <NavLink to="/SubmitArticle">Submit an Article</NavLink>
+          </li>
+          <li>
+            <NavLink to="/ModeratorPage">Moderator Page</NavLink>
+          </li>
+          <li>
+            <NavLink to="/TableTemplate">Analyst Page</NavLink>
+          </li>
+        </ul>
+        <div className="content">
+          <Route exact path="/" component={Home} />
+          <Route path="/SEPractice" component={SEPractice} />
+          <Route path="/SubmitArticle" component={SubmitArticle} />
+          <Route path="/ModeratorPage" component={ModeratorPage} />
+          <Route path="/TableTemplate" component={TableTemplate} />
+        </div>
       </div>
-    )
-  }
-}
+    </Router>
+  );
+};
 
 export default App;
