b89e74424cd1ea75920ba76f21d597d42761f4af
Jiali Lu <jialilyv@gmail.com>
2022-06-01 12:17:04 +0800

    tidyup app.test

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -3,19 +3,6 @@
 import react from 'react';
 
 import {wrapper} from 'enzyme';
-// test('renders learn react link', () => {
-//   render(<App />);
-//   const linkElement = screen.getByText(/learn react/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
-
-// it('renders without crashing', () =>{
-//   const div = document.createElement('div');
-//   ReactDOM.render(<App/>, div);
-//   ReactDOM.unmountComponentAtNode(div);
-  
-// })
-
 
 
 const SubmissionForm = require('./components/SubmissionForm');
