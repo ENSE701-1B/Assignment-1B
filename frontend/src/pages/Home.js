66f605771a0e4c5ea4c8b63752bf15998a7deaa4
wsking233 <2310016713@qq.com>
2022-06-02 19:13:47 +1200

    Final update

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -8,13 +8,45 @@ class Home extends Component {
   render() {
     
     return (
-      <div>
-        
-        <h1> Welcome to the SPEED repository!</h1>
-        <p> Please go to the Submitter An Article Page to view articles.
-            Alternatively, if you are a Moderator or an Analyst, please 
-            head to the Moderator Page or Analyst Page</p> 
-          
+      <div class="main">
+        <h1> ENSE701-Contemporary Issues in Software Engineering</h1>
+        <h3>Team Members:</h3>
+      <div class="home">
+
+        <div class="card">
+            <p>
+              Name:JiaXu Li<br></br>
+              ID: 18021885<br></br>
+              Email:<a href="mailto:hht3280@autuni.ac.nz">hht3280@autuni.ac.nz</a>
+            </p>
+          </div>
+
+          <div class="card">
+            <p>
+              Name: JiaLi Lu<br></br>
+              ID: 21135614<br></br>
+              Email:<a href="mailto:rfp0761@autuni.ac.nz">rfp0761@autuni.ac.nz</a>
+            </p>
+          </div>
+  
+          <div class="card">
+            <p>
+              Name: William Wang<br></br>
+              ID: 18017970<br></br>
+              Email:<a href="mailto:xwg1585@autuni.ac.nz">xwg1585@autuni.ac.nz</a>
+            </p>
+          </div>
+      
+          <div class="card">
+            <p>
+              Name: RuYang Xu<br></br>
+              ID: 18017865<br></br>
+              Email:<a href="mailto:jfm7532@autuni.ac.nz">jfm7532@autuni.ac.nz</a>
+            </p>
+          </div>
+    
+      </div>
+
       </div>
     );
   }
